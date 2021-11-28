8df35a636ab2bf86bda4d619347f3ddab4120474
Akash Veer <akashveer98@gmail.com>
2021-11-28 12:38:31 +0530

    Hello-world code push

---
+++ Receiver/Receiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { Uri = new Uri(
+                "amqp://guest:guest@localhost:5672"
+                )};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("demo-queue", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sender/Sender/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Sender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri(
+                "amqp://guest:guest@localhost:5672"
+                )
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("demo-queue", false, false, false, null);
+
+            var message = "Hello Akash!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", "demo-queue", null, body);
+
+        }
+    }
+}
